8eaa64850ad51e784db93f1293b6ab57ea014d05
BobNobrain <nikita-ivanov.mail@yandex.ru>
2017-11-21 20:16:31 +0300

    Removed unnesessary anorm.js, stub for Repo and imports for src/index.js

---
+++ src/Repo.js
@@ -0,0 +1,4 @@
+// TODO: models repository (very far-in-the-future todo)
+export default class Repo {
+
+}
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import { Model, model } from './Model';
+import Repo from './Repo';
+
+// rename and reexport
+export default {
+    Model: model,
+    TModel: Model,
+    Repo
+};
